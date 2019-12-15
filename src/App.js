ff853d3846f1fe2b78ed45cb2c72b74179253bfe
Brian <briankeefe@umass.edu>
2019-12-15 19:05:51 +0000

    border color

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import {
 	Grid,
 	AppBar,
 	Toolbar,
-	Button
+	Button,
+	ButtonGroup
 } from "@material-ui/core";
 import { blue } from "@material-ui/core/colors";
 import space from "./images/space.jpg";
@@ -19,9 +20,11 @@ function App() {
 			<AppBar className="AppBar" position="static">
 				<Toolbar style={{ flexGrow: "1" }}>
 					<Box style={{ flexGrow: "1" }} />
-					<Button>Github</Button>
-					<Button>Social</Button>
-					<Button>Projects</Button>
+					<ButtonGroup id="buttons" variant="text">
+						<Button>Github</Button>
+						<Button>Social</Button>
+						<Button>Projects</Button>
+					</ButtonGroup>
 				</Toolbar>
 			</AppBar>
 			<Box className="outer">
