bd2ff2fcbd9fb04a0aea7db390332d26f0529550
Brian <briankeefe@umass.edu>
2019-12-15 21:49:19 +0000

    formatted card

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Box, Card, CardContent, Typography, Grid } from "@material-ui/core";
+import {
+	Box,
+	Card,
+	CardContent,
+	Typography,
+	Grid,
+	Paper
+} from "@material-ui/core";
 import { green } from "@material-ui/core/colors";
 import TopBar from "./layout/TopBar";
 import SimpleSlider from "./SimpleSlider";
@@ -10,19 +17,21 @@ let ProjectPage = () => {
 	return (
 		<Box className="projects-page" style={{ backgroundColor: green[100] }}>
 			<TopBar />
-			<Grid container justify="center" direction="column" alignItems="center">
-				{projectNames.map(e => {
-					return (
-						<Grid item xs={12} sm={9} md={6}>
-							<Card style={{ height: "60vh" }}>
-								<Grid container justify="center">
-									<Typography variant="h5">{e}</Typography>
-								</Grid>
-							</Card>
+			{projectNames.map(e => {
+				return (
+					<Grid container justify="center">
+						<Grid item xs={12} md={6}>
+							<Paper className="project-card">
+								<Box className="box">
+									<Grid container justify="center">
+										<Typography variant="h5">{e}</Typography>
+									</Grid>
+								</Box>
+							</Paper>
 						</Grid>
-					);
-				})}
-			</Grid>
+					</Grid>
+				);
+			})}
 		</Box>
 	);
 };
