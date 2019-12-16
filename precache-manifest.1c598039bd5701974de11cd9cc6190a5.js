29468f3c9f1288d513e25ff009e937d6e0de331c
Brian <briankeefe@umass.edu>
2019-12-16 16:35:26 +0000

    Updates

---
+++ precache-manifest.1c598039bd5701974de11cd9cc6190a5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2118cce6e0b2554952485c757656d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63000bb27097792441d4",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "e93e45d883379f5713b2",
+    "url": "/static/css/main.a2f0508c.chunk.css"
+  },
+  {
+    "revision": "63000bb27097792441d4",
+    "url": "/static/js/2.f635774e.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.f635774e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e93e45d883379f5713b2",
+    "url": "/static/js/main.67171782.chunk.js"
+  },
+  {
+    "revision": "a796a236ee8470d38315",
+    "url": "/static/js/runtime-main.783779e9.js"
+  },
+  {
+    "revision": "8d18551fe363c2817144474e9038a872",
+    "url": "/static/media/Cascadia.8d18551f.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a4eb5f9cc39e4bbbdb57b063cdad863e",
+    "url": "/static/media/space.a4eb5f9c.jpg"
+  }
+]);
