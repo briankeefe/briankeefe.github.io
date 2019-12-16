8a4f1c79d4218baedc937135623774844d7f5157
Brian <briankeefe@umass.edu>
2019-12-16 07:31:13 +0000

    squashed formatting bug and added to about

--- src/About.js
+++ src/About.js
@@ -1,12 +1,57 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Grid, Card, CardContent } from "@material-ui/core";
 import { blue } from "@material-ui/core/colors";
 import TopBar from "./layout/TopBar";
+import internships from "./content/internships";
 let AboutPage = () => {
 	return (
-		<Box className="resume-page" style={{ backgroundColor: blue[100] }}>
+		<Box className="about-page" style={{ backgroundColor: blue[50] }}>
 			<TopBar />
-			<Typography>Testing</Typography>
+			<Box p={2}>
+				<Grid container spacing={2} justify="center">
+					<Grid item xs={12}>
+						<Typography
+							className="cascadia"
+							style={{ textAlign: "center" }}
+							variant="h4"
+						>
+							About me
+						</Typography>
+					</Grid>
+					<Grid item xs={4}>
+						<Card>
+							<CardContent>
+								<Typography variant="h5">Education</Typography>
+								<Typography variant="body1">
+									University of Massachusetts Amherst
+								</Typography>
+								<Typography variant="body1">Graduating Class: 2020</Typography>
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={8}>
+						<Card>
+							<CardContent>
+								<Typography variant="h5"> Professional Experience</Typography>
+								{internships.map(e => {
+									return (
+										<Box>
+											<Typography variant="p">
+												<b>{e.name}</b>
+											</Typography>
+											<Typography variant="p"> - {e.position}</Typography>
+											<Typography display="block" variant="p">
+												{e.desc}
+											</Typography>
+											<Box mb={2} />
+										</Box>
+									);
+								})}
+							</CardContent>
+						</Card>
+					</Grid>
+				</Grid>
+			</Box>
 		</Box>
 	);
 };
---
+++ src/content/internships.js
@@ -0,0 +1,22 @@
+let internships = [
+	{
+		name: "Optum, Boston MA",
+		position: "Software Engineering Intern (Full Stack)",
+		desc:
+			"Worked collaboratively with five other interns to create a full stack web portal. In order to achieve a polished finished product, our team worked directly with the customer service representatives and established a feedback loop for constant improvements. Once we understood the needs of the user, we designed an interface in .NET Core Razor that queried secure information from a Microsoft SQL database and displayed it in an easy to read manner for customer service representatives. We then deployed this web app to AWS with functioning auto-scaling policy, and implemented end-to-end testing."
+	},
+	{
+		name: "Staples, Framingham MA",
+		position: "Software Engineering Intern (DevOps)",
+		desc:
+			"Worked alongside full time developers as the sole summer intern of the Retail Technology Development Team. Was introduced to an agile project management environment and learned several key skills necessary to address technical issues and implement solutions. Met with colleagues daily in different environments to effectively communicate progress while setting and understanding personal and team goals. Documented my work to be replicated by colleagues and trained members of other trains on proper implementation."
+	},
+	{
+		name: "Sunshine Systems, Southborough MA",
+		position: "Software Engineering Intern (Backend)",
+		desc:
+			"Analyzed a predated time-punch program written in Visual Basic and built a new system in Java. Trained on proper programming techniques while simultaneously learning both programming languages by exploring and utilizing resources such as books, texts, and tutorials. "
+	}
+];
+
+export default internships;
